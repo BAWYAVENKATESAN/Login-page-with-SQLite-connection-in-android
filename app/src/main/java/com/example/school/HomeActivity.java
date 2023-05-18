afc38f5adf4aad23536668be53b54b0eed1b866f
BAWYAVENKATESAN <bawya.se21@bitsathy.ac.in>
2023-05-18 10:16:27 +0530

    first commit

---
+++ app/src/main/java/com/example/school/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.school;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+
+}
+
